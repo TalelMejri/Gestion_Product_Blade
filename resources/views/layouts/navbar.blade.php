f3daba4259102000fabe82a08cd0269c98dbc34b
talel <talelmejri8@gmail.com>
2023-03-19 00:49:45 +0100

    navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,15 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid text-center">
+      <a class="navbar-brand" href="#">Crud Products</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
+          <a class="nav-link" href="/produits">List Products</a>
+        </div>
+      </div>
+    </div>
+  </nav>
