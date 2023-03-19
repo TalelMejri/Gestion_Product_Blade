7bda8c6893dc80461e77b09abf2ec306dc6ee3a3
talel <talelmejri8@gmail.com>
2023-03-19 13:04:28 +0100

    fix image show

--- resources/views/Produits/show.blade.php
+++ resources/views/Produits/show.blade.php
@@ -5,7 +5,7 @@
 <div class="card mb-3 " style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
+        <img src="{{Storage::URL($produit->photo)}}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
