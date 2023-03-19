24db769085ee62c450478bd1df6d226e6c12c33c
talel <talelmejri8@gmail.com>
2023-03-19 13:04:20 +0100

    fic display image

--- resources/views/Produits/index.blade.php
+++ resources/views/Produits/index.blade.php
@@ -1,11 +1,14 @@
 
 @extends("layouts.App")
 @section("content")
-<div class="container mt-5">
-    <form action="/produits" method="get" class="d-flex gap-2">
+<div class="container-fluid mt-5 row text-center">
+    <form action="/produits" method="get" class="d-flex gap-2 text-center col-lg-6">
         <input type="text" name="search" class="form-control w-50"  placeholder="Search ...">
         <button type="submit" class="btn btn-outline-success">Search</button>
     </form>
+    <span class="col-lg-6">
+         <a href="{{url('produits/create')}}"><button class="btn btn-primary">Add Product</button></a>
+    </span>
 </div>
 <div class="container w-50 mt-2">
     @if (isset($message_sucess))
@@ -19,13 +22,13 @@
     </div>
    @endif
 </div>
-
-
-
 <table class="table container mt-2">
     <thead class="table-dark">
        <th>
           #
+       </th>
+        <th>
+          Photo
        </th>
        <th>
          Reference
@@ -49,7 +52,7 @@
     @if (count($produits)==0)
     <tbody>
         <tr>
-            <td colspan="7" class="text-center bg-danger text-white">No data</td>
+            <td colspan="8" class="text-center bg-danger text-white">No data</td>
         </tr>
     </tbody>
     @else
@@ -57,6 +60,7 @@
         @foreach ($produits as $prod )
       <tr>
             <td>{{$prod->id}}</td>
+            <td><img  width="100px" height="100px" src="{{Storage::URL($prod->photo)}}"></td>
             <td>{{substr($prod->reference,0,15).'...'}}</td>
             <td>{{substr($prod->designation,0,15).'...'}}</td>
             <td>{{$prod->quantite}}</td>
@@ -64,7 +68,7 @@
             <td>{{$prod->user->name}}</td>
             <td>
                 <a href="{{route('produits.show',$prod->id)}}"><button class="btn btn-primary">Show</button></a>
-                <a href=""><button class="btn btn-warning">Edit</button></a>
+                <a href="{{route('findbyId',$prod->id)}}"><button class="btn btn-warning">Edit</button></a>
                  <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$prod->id}}">Delete</button>
 
   <div class="modal fade" id="staticBackdrop{{$prod->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -90,9 +94,32 @@
   </div>
             </td>
       </tr>
+
       @endforeach
     </tbody>
     @endif
   </table>
+  {{-- <div style="height: 18px" class=" text-center mt-4   d-inline">
+       <p > {{$produits->links()}}</p>
+  </div> --}}
+  {{-- <div class="row">
+    <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          @for ($num=1  ; $num<=(ceil($produits->perPage() ?  $produits->total()/$produits->perPage() : 1));$num++ )
+          <li  class="page-item @if ($num==$produits->currentPage()) ? active : '' @endif" aria-current="page">
+            <a class="page-link" href="#">{{$num}}</a>
+          </li>
+          @endfor
+          <li class="page-item">
+            <a class="page-link" href="">Next</a>
+          </li>
+        </ul>
+      </nav>
+        {{echo explode($produits->nextPageUrl(),"=")}}
+        {{$produits->previousPageUrl()}} --}}
+ </div>
 
 @endsection
