a90f77cc6005adf324c782b891b07c942836a217
talel <talelmejri8@gmail.com>
2023-03-19 13:04:39 +0100

    add route find with name

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 
 Route::resource('/produits',ProduitController::class);
+Route::get('/produits/find/{id}',[ProduitController::class,'edit'])->name('findbyId');
